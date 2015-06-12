89fccf007eb75e710b197e4e6e9908a2aa43e734
FrankiezZZ <zhenghm48@163.com>
2015-06-12 09:32:35 +0800

    Initial Commit

---
+++ 01-NSSession/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  01-NSSession
+//
+//  Created by 汤蓉 on 15/6/12.
+//  Copyright (c) 2015年 zhm. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
